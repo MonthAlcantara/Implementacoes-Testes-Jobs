e8a65db7cca3ebc3a137537eae3f310331fce528
Montival <montival_junior@yahoo.com.br>
2020-09-15 10:56:53 -0300

    criacao projeto menu

---
+++ menu/src/test/java/io/month/alcantara/menu/MenuApplicationTests.java
@@ -0,0 +1,13 @@
+package io.month.alcantara.menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
